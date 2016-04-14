1e1a0e08f88a493d7459a9008543223463b25207
Thomas Jang <tom@axisj.com>
2016-04-14 21:03:44 +0900

    init

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var changed = require('gulp-changed');
+var plumber = require('gulp-plumber');
+var notify = require("gulp-notify");
+var clean = require('gulp-clean');
+
+
+function errorAlert(error) {
+    notify.onError({title: "Gulp Error", message: "Check your terminal", sound: "Purr"})(error); //Error Notification
+    console.log(error.toString());//Prints Error to Console
+    this.emit("end"); //End function
+}
+
+/**
+ * for JS
+ */
+gulp.task('js', function () {
+    gulp.src('src/*.js')
+        .pipe(plumber({errorHandler: errorAlert}))
+        .pipe(concat('jquery.history.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(concat('jquery.history.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+/**
+ * watch
+ */
+gulp.task('default', function () {
+    gulp.watch('src/*.js', ['js']);
+});
